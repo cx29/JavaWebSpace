925f59fe687203d64453eeb4f2cd0fd17a46497e
cx29 <1185366392@qq.com>
2022-03-22 17:17:58 +0800

    initial project

---
+++ SunShineAirLines/src/main/java/cn/tn/service/UserService.java
@@ -0,0 +1,5 @@
+package cn.tn.service;
+
+public interface UserService {
+    void save();
+}
---
+++ SunShineAirLines/src/main/java/cn/tn/service/impl/UserServiceImpl.java
@@ -0,0 +1,10 @@
+package cn.tn.service.impl;
+
+import cn.tn.service.UserService;
+
+public class UserServiceImpl implements UserService {
+    @Override
+    public void save() {
+        System.out.println(2222222);
+    }
+}
